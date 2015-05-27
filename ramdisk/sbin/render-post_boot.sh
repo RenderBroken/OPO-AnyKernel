2c9ab31d5661c2f7752188c69f77da2e2f838f27
Zachariah <zkennedy87@gmail.com>
2015-05-26 22:34:36 -0400

    Render Script: update
    Signed-off-by: Zachariah <zkennedy87@gmail.com>

--- ramdisk/sbin/render-post_boot.sh
+++ ramdisk/sbin/render-post_boot.sh
@@ -138,14 +138,43 @@ echo "[Render-Kernel] disable debug mask" | tee /dev/kmsg
 ############################
 # KCAL Tunings Thanks AK!
 #
-echo "256 256 256" > /sys/devices/platform/kcal_ctrl.0/kcal
+echo "250 250 255" > /sys/devices/platform/kcal_ctrl.0/kcal
 echo "35" > /sys/devices/platform/kcal_ctrl.0/kcal_min
 echo "1" > /sys/devices/platform/kcal_ctrl.0/kcal_enable
 echo "0" > /sys/devices/platform/kcal_ctrl.0/kcal_invert
-echo "275" > /sys/devices/platform/kcal_ctrl.0/kcal_sat
+echo "285" > /sys/devices/platform/kcal_ctrl.0/kcal_sat
 echo "0" > /sys/devices/platform/kcal_ctrl.0/kcal_hue
-echo "251" > /sys/devices/platform/kcal_ctrl.0/kcal_val
-echo "258" > /sys/devices/platform/kcal_ctrl.0/kcal_cont
+echo "245" > /sys/devices/platform/kcal_ctrl.0/kcal_val
+echo "264" > /sys/devices/platform/kcal_ctrl.0/kcal_cont
 echo "[Render-Kernel] kcal sgs amoled settings done" | tee /dev/kmsg
+
+############################
+# Init.d Support
+# 
+/sbin/busybox run-parts /system/etc/init.d
+
+############################
+# SuperUser Tweaks (TESTING)
+# Allow untrusted apps to read from debugfs
+if [ -e /system/lib/libsupol.so ]; then
+/system/xbin/supolicy --live \
+	"allow untrusted_app debugfs file { open read getattr }" \
+	"allow untrusted_app sysfs_lowmemorykiller file { open read getattr }" \
+	"allow untrusted_app persist_file dir { open read getattr }" \
+	"allow debuggerd gpu_device chr_file { open read getattr }" \
+	"allow netd netd capability fsetid" \
+	"allow netd { hostapd dnsmasq } process fork" \
+	"allow { system_app shell } dalvikcache_data_file file write" \
+	"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
+	"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
+	"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }" \
+	"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }" \
+	"allow system_server dex2oat_exec file rx_file_perms" \
+	"allow mediaserver mediaserver_tmpfs file execute" \
+	"allow drmserver theme_data_file file r_file_perms" \
+	"allow zygote system_file file write" \
+	"allow atfwd property_socket sock_file write" \
+	"allow debuggerd app_data_file dir search"
+fi;
 
 echo "[Render-Kernel] Boot Script Completed!" | tee /dev/kmsg
